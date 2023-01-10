e8916c32ec3454af2d601b532113c45cd5a851fe
unknown <ankh>
2023-01-10 20:18:57 +0800

    添加测试类

--- src/test/java/host/ankh/jeyseylearn/JAXRSResourceBase.java
+++ src/test/java/host/ankh/jeyseylearn/JAXRSResourceBase.java
@@ -7,21 +7,29 @@ import jakarta.ws.rs.core.Form;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.HttpUrlConnectorProvider;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Map;
+import java.util.function.Supplier;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JAXRSResourceBase {
     @Value("${local.server.port}")
     int port;
+
+    Logger log = LoggerFactory.getLogger(JAXRSResourceBase.class);
 
     private Map<String, NewCookie> cookies;
 
     Invocation.Builder build(String path) {
         Invocation.Builder builder = ClientBuilder.newClient()
                 .target("http://localhost:" + port + "/api" + path)
+                .property(ClientProperties.FOLLOW_REDIRECTS, false)
                 .request(MediaType.MULTIPART_FORM_DATA).accept(MediaType.TEXT_PLAIN);
         if (cookies != null) {
             // 如果cookies不为空，说明已经登录，把cookies都传进去
@@ -36,7 +44,11 @@ public class JAXRSResourceBase {
         Form form = new Form();
         form.param("username", "user");
         form.param("password", "user");
-        var res = post("/login", form);
+        Invocation.Builder builder = ClientBuilder.newClient()
+                .target("http://localhost:" + port + "/login")
+                .property(ClientProperties.FOLLOW_REDIRECTS, false)
+                .request(MediaType.MULTIPART_FORM_DATA).accept(MediaType.TEXT_PLAIN);
+        var res = builder.post(Entity.form(form));
         this.cookies = res.getCookies();
     }
 
@@ -55,7 +67,21 @@ public class JAXRSResourceBase {
     }
 
     Response get(String path) {
-        return build(path).get();
+        var res = build(path).get();
+//        log.error(new Supplier<String>() {
+//            String ANSI_RESET = "\u001B[0m\n";
+//            String ANSI_BLUE = "\u001B[34m";
+//            @Override
+//            public String get() {
+//                var sb = new StringBuilder();
+//                sb.append(ANSI_BLUE);
+//                sb.append(res.toString());
+//                sb.append(res.readEntity(String.class));
+//                sb.append(ANSI_RESET);
+//                return sb.toString();
+//            }
+//        });
+        return res;
     }
 
     Response post(String path, Object entity) {
@@ -68,6 +94,9 @@ public class JAXRSResourceBase {
     }
     static void assertForbidden(Response response) {
         assert Response.Status.FORBIDDEN.getStatusCode() == response.getStatus();
+    }
+    static void assertRedirect(Response response) {
+        assert Response.Status.FOUND.getStatusCode() == response.getStatus();
     }
     static void assertOK(Response response) {
         assert Response.Status.OK.getStatusCode() == response.getStatus();
--- src/test/java/host/ankh/jeyseylearn/JerseyLearnApplicationTests.java
+++ src/test/java/host/ankh/jeyseylearn/JerseyLearnApplicationTests.java
@@ -1,37 +1,42 @@
 package host.ankh.jeyseylearn;
 
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class JerseyLearnApplicationTests {
-	@Inject
-	private TestRestTemplate restTemplate;
+import java.util.function.Supplier;
+
+class JerseyLearnApplicationTests extends JAXRSResourceBase {
+
 
 	@Test
-	void hello() {
-		ResponseEntity<String> entity = this.restTemplate.getForEntity("/api/hello", String.class);
-		assert(entity.getStatusCode().is2xxSuccessful());
-		assert(entity.getBody().equals("hello from spring"));
+	void helloWhenUnauthenticated() throws Exception {
+		var res = get("/hello");
+		assertRedirect(res);
 	}
 
 	@Test
-	void reverse() {
-		ResponseEntity<String> entity = this.restTemplate.getForEntity("/api/reverse?data=hello", String.class);
-		assert entity.getStatusCode().is2xxSuccessful();
-		assert entity.getBody().equals("olleh");
+	void helloWhenAuthenticated() throws Exception {
+		authenticatedScope(() -> {
+			var res = get("/hello");
+			assertOK(res);
+			var body = res.readEntity(String.class);
+			log.debug(new Supplier<String>() {
+				@Override
+				public String get() {
+					return "\u001B[34m" + body + "\u001B[0m\n" ;
+				}
+			});
+			assert body.equals("hello from spring");
+		});
 	}
 
 	@Test
-	void validation() {
-		ResponseEntity<String> entity = this.restTemplate.getForEntity("/api/reverse", String.class);
-
-		// FIXME: 这里直接跳转到login界面了，应该改成404返回
-//		assert entity.getStatusCode().is4xxClientError();
+	void reverseWithParamsWhenAuth() throws Exception {
+		authenticatedScope(() -> {
+			var res = get("/reverse?data=hello");
+			assertOK(res);
+			assert res.readEntity(String.class).equals("olleh");
+		});
 	}
-
-
 }
